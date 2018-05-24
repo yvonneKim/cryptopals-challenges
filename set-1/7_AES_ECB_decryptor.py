b66fba8b7d831055b1cc7562f8e2d6b144911c74
yvonneKim <yhk@utexas.edu>
2018-05-24 04:12:35 -0500

    Stil working on decrypting properly

--- set-1/7_AES_ECB_decryptor.py
+++ set-1/7_AES_ECB_decryptor.py
@@ -1,4 +1,4 @@
-# I took this mostly from: https://cryptography.io/en/latest/hazmat/primitives/symmetric-encryption/#cryptography.hazmat.primitives.ciphers.algorithms.AES
+# I took this mostly from: https://cryptography.io/en/latest/hazmat/primitives/symmetric-encryption/#cryptography.hazmat.primitives.ciphers.algorith1;2802;0cms.AES
 import os, sys, base64
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
@@ -13,20 +13,21 @@ backend = default_backend()
 bsize = len(key)
 num_blocks = (len(data) + bsize -1) // bsize
 if len(data) % bsize != 0:
+    print("PADDING NOW")
     data = data.ljust(num_blocks, bytes(len(data) % bsize)[0])
 
 ptext_blocks = []
 # decryption block by block backwards n-1 times (stop at 2nd block)
-block_gen = lambda d, b: ((d[i-b:i], d[i:i+b]) for i in range(b*num_blocks, 0, -b))
+block_gen = lambda d, b: ((d[i-(2*b):i-b], d[i-b:i]) for i in range(b*num_blocks, b, -b))
 for blocks in block_gen(data, bsize):
     iv = blocks[0]
     ctext = blocks[1]
-    
-    cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=backend)
-    decryptor = cipher.decryptor
-    r = decryptor.update(ctext) + decryptor.finalize()
-    ptext_blocks.append(r)
 
+    cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=backend)
+    decryptor = cipher.decryptor()
+    ptext = decryptor.update(ctext) + decryptor.finalize()
+    ptext_blocks.append(ptext)
 
 with open(sys.argv[1]+'.out', 'wb') as f:
-    f.write(b''.join(ptext_blocks))
+    out = b''.join(ptext_blocks)
+    f.write(out)
