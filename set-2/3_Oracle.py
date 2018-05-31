f6020d8024843cc2340bec15e911405d639b819a
yvonneKim <yhk@utexas.edu>
2018-05-30 21:36:26 -0500

    Finished ECB/CBC oracle. Only works on larger files

--- set-2/3_Oracle.py
+++ set-2/3_Oracle.py
@@ -8,13 +8,13 @@ from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
 
 def __CBC_encrypt(ptext, key):
-    print("CBC")
+    print("Encrypting with CBC...")
     iv = os.urandom(16)
     cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())
     return cipher.encryptor().update(ptext)
     
 def __ECB_encrypt(ptext, key):
-    print("ECB")
+    print("Encrypting with ECB...")
     cipher = Cipher(algorithms.AES(key), modes.ECB(), backend=default_backend())
     return cipher.encryptor().update(ptext)
 
@@ -24,17 +24,19 @@ def random_encrypt(ptext):
     # Beforehand, pads the data with 1-15 (random) bytes both before and after.
     
     key = os.urandom(16) # has to be 16 for the challenge
-    ptext = os.urandom(random.randint(5, 10)) + ptext + os.urandom(random.randint(5, 10))
+    front = os.urandom(random.randint(5, 10))
+    back = os.urandom(random.randint(5, 10))
+                       
+    ptext = front + ptext + back
     ptext = PKCS7.pad(ptext, len(key))
     algos = {0: __CBC_encrypt, 1: __ECB_encrypt}
     return algos[random.randint(0, 1)](ptext, key)
 
 def analyze(data):
-    bsize = 2
+    bsize = 8
     # what size "blocks" to use for frequency analysis? start with 16?
     blocks = Counter([data[i:i+bsize] for i in range(0, len(data)-bsize+1, bsize)])
-    print(blocks.most_common(100))
-    if blocks.most_common(1)[0][1] >= 3: # adjust this number?
+    if blocks.most_common(1)[0][1] >= 5: # adjust this number?
         return 'ECB'
     return 'CBC'
 
