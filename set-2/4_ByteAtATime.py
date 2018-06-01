f63df7e82e534a5bfa1c2d7e603a6e51bb3135ff
yvonneKim <yhk@utexas.edu>
2018-06-01 11:54:10 -0500

    Added block size determiner

--- set-2/4_ByteAtATime.py
+++ set-2/4_ByteAtATime.py
@@ -8,11 +8,9 @@ from cryptography.hazmat.backends import default_backend
 
 def main():
     global_key = os.urandom(16)
-    data = sys.argv[1].encode('utf-8')
+    data = b'YELLOW SUBMARINE'
     e = encrypt(data, global_key)
-    print(e)
-    d = decryptMsg(e)
-    print(d)
+    d = decrypt_msg(e, global_key)
 
 def encrypt(data, key):
     secret = """
@@ -23,7 +21,32 @@ def encrypt(data, key):
     cipher = Cipher(algorithms.AES(key), modes.ECB(), backend=default_backend())
     return cipher.encryptor().update(data)
 
-def decryptMsg(data):
+def decrypt_msg(data, key):
+    # Determine blocksize. Most likely 16 bytes.
+    # The first i that results in a repeated first block is (bsize + 1)
+
+    def get_bsize(data):
+        prev = None
+        for i in range(2, 32):
+            x = encrypt(b'A'*i, key)
+            if prev == None: # first time
+                prev = x[0:i]
+            elif prev == x[0:len(prev)]:
+                return i - 1
+            else:
+                print(prev)
+                prev = x[0:i]
+            
+    bsize = get_bsize(data)
+    print(bsize)
+
+    d = Oracle.analyze(data, bsize)
+    if d == 'ECB':
+        pass
+    elif d == 'CBC':
+        pass
+    else:
+        pass
     return data
 
 if __name__=='__main__':
