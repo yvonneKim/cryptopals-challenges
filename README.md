297fabb584015830617c0ab5ec59216cb9c56c0a
Yvonne Kim <yhk@utexas.edu>
2018-10-06 18:22:57 -0500

    Added a readme

---
+++ README.md
@@ -0,0 +1,20 @@
+#Cryptopals Challenges
+###Set 1
+1. ~~Convert hex to base64~~
+2. ~~Fixed XOR~~
+3. ~~Single-byte XOR cipher~~
+4. ~~Detect single-character XOR~~
+5. ~~Implement repeating-key XOR~~
+6. ~~Break repeating-key XOR~~
+7. ~~AES in ECB mode~~
+8. ~~Detect AES in ECB mode~~
+
+###Set 2
+1. ~~Implement PKCS#7 padding~~
+2. ~~Implement CBC mode~~
+3. ~~An ECB/CBC detection oracle~~
+4. ~~Byte-at-a-time ECB decryption (Simple)~~
+5. ~~ECB cut-and-paste~~
+6. ~~Byte-at-a-time ECB decryption (Harder)~~
+7. ~~PKCS#7 padding validation~~
+8. CBC bitflipping attacks
--- set-2/8_CBC_Bitflipping.py
+++ set-2/8_CBC_Bitflipping.py
@@ -1,61 +1,73 @@
-import sys, os, base64, random
-sys.path.append(os.path.abspath('../cryptocommon'))
-
-PKCS7 = __import__('PKCS7')
-CBC = __import__('AES_CBC')
+import sys
+import os
+import base64
+import random
 from byteXor import xor as bxor
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
 
+sys.path.append(os.path.abspath("../cryptocommon"))
+
+PKCS7 = __import__("PKCS7")
+CBC = __import__("AES_CBC")
+
+bsize = 16  # by default
+key = os.urandom(bsize)
+iv = b"\x00" * bsize
+
 
 def main():
     # process cmd arguments- a file with the text to encrypt
-    with open(sys.argv[1], 'r') as f:
-        data = ''.join([x.strip() for x in f.readlines()])
-    
-    bsize = 16 # by default
-    key = os.urandom(bsize)
+    with open(sys.argv[1], "r") as f:
+        data = "".join([x.strip() for x in f.readlines()])
 
-    # add the pre and post strings
-    pre = "comment1=cooking%20MCs;userdata="
-    post = ";comment2=%20like%20a%20pound%20of%20bacon"    
-    predata = (pre+data+post)
-
-    # quoting out ; and = make it impossible to inject ";admin=true;"in the input data    
-    data = predata.replace(';', '";"').replace('=', '"="')
-
-    # padding 
-    data = PKCS7.padPKCS7(data.encode('utf-8'), bsize)
-    predata = PKCS7.padPKCS7(predata.encode('utf-8'), bsize)
-
-    print("First block:")
-    print(data[:16])
-    print()
-
-    print("Desired first block:")
-    print(predata[:16])
-    print()
-
-    # get XOR mask by XORing first block w/ desired first block
-    xor_mask = bxor(data[:16], predata[:16])
-    print("XOR mask:")
-    print(xor_mask)
-    print()
-
-    print("XOR'd to: ")
-    result = bxor(data[:16], xor_mask)
-    print(result)
-    print()
-
-    print("Does this match our desired block?")
-    print(result == predata[:16])
-
-    # encryption with iv of all zeroes
-    encrypted = CBC.encrypt(data, key, iv = b'\x00' * bsize)
+    modified_data, e = encrypt(data)
+    desired_data = modified_data.replace('"', "")
+    difference = len(e) - len(desired_data)
+    desired_data = desired_data + (difference * "x")
+    d = decrypt(e, desired_data.encode("utf-8"))
 
     # output to file
     # with open(sys.argv[1]+'.out', 'wb') as f:
     #     f.write(ptext)
 
-if __name__=='__main__':
+
+def encrypt(data):
+    """ Takes in a plaintext string, quotes out ; and = chars,
+        PKCS7 pads it, and CBC encrypts tiem.
+        Returns the modified string and it's encryption.
+    """
+    # quoting out ; and = make it impossible to inject ";admin=true;"
+    data = data.replace(";", '";"').replace("=", '"="')
+
+    # add the pre and post strings
+    pre = "comment1=cooking%20MCs;userdata="
+    post = ";comment2=%20like%20a%20pound%20of%20bacon"
+    data = pre + data + post
+
+    # padding
+    padded_data = PKCS7.padPKCS7(data.encode("utf-8"), bsize)
+
+    # encryption with iv of all zeroes
+    encrypted = CBC.encrypt(padded_data, key, iv)
+
+    return data, encrypted
+
+
+def decrypt(data, desired):
+    dat = data[len(data) - bsize:len(data)]
+    result = [dat]
+    for i in range(len(data), 0, -bsize):
+        print("Result for iteration " + str(i) + " is: " + str(result))
+        des = desired[i - bsize:i]
+        decrypted_dat = CBC.decrypt(dat, key, iv)
+        dat = bxor(des, dat)
+        result.append(dat)
+
+    result.reverse()
+    result = b"".join(result)
+    return CBC.decrypt(result, key, iv)
+
+
+if __name__ == "__main__":
     main()
