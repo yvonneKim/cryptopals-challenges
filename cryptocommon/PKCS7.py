f556ddbf75a4d4560e7611acbf2ae444955f360d
Yvonne Kim <yhk@utexas.edu>
2018-10-07 19:00:39 -0500

    Forgot a few files

--- cryptocommon/PKCS7.py
+++ cryptocommon/PKCS7.py
@@ -3,21 +3,29 @@
 
 import math
 
+
 def isPaddedPKCS7(text, bsize):
     """ Takes in plaintext as bytes and blocksize and returns whether it has valid PKCS7 padding.
         Input where there is no padding is considered valid. (\x00 * 0 times)
         Input that is not a multiple of bsize is considered invalid.
     """
-    text = text.encode('utf-8')
+    if type(text) is str:
+        text = text.encode('utf-8')
+
+    if type(text) is not bytes:
+        raise TypeError('Is neither string nor bytes!')
 
     if len(text) % bsize != 0:
         return False
 
     pad_byte = text[-1:]
-    pad_byte_as_int = int(pad_byte)
+    pad_byte_as_int = int.from_bytes(pad_byte, 'big', signed=True)
+    if pad_byte_as_int > 15:
+        return True
+
     padding = (text[-pad_byte_as_int:])
-    
-    return padding == pad_byte*pad_byte_as_int    
+    return padding == pad_byte*pad_byte_as_int
+
 
 def stripPaddingPKCS7(text, bsize):
     """ Takes in plaintext as bytes and blocksize, returning the text without PKCS7 padding.
@@ -27,15 +35,17 @@ def stripPaddingPKCS7(text, bsize):
 
     return text[:-pad_length]
 
+
 def stripIfPaddedPKCS7(text, bsize):
     """ Combines checking and stripping for PKCS7 padding.
         Raises ValueError if the text does not have PKCS7 padding.
     """
 
-    if isPaddedPKCS7(text, bsize): 
+    if isPaddedPKCS7(text, bsize):
         return stripPaddingPKCS7(text, bsize)
 
     raise ValueError('Padding is invalid!')
+
 
 def padPKCS7(data, bsize):
     """ Takes text as bytes and PKCS7 pads it. """
